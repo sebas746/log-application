98282b4895dc84fb227cc955959ecaef08fba0d6
v-jortegon <v-jortegon@youngliving.com>
2021-10-19 10:08:31 -0500

    Deleted not used references.

--- LogApplication.WebApi/Startup.cs
+++ LogApplication.WebApi/Startup.cs
@@ -1,20 +1,12 @@
 using LogApplication.WebApi.Helpers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using NLog;
 using NLog.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LogApplication.WebApi
 {
